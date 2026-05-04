888b877e677daded80d4d5b3860a0536025f6fbe
nikosmpi <nikosmpi@gmail.com>
2026-05-04 15:06:03 +0000

    allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		plugins: [sveltekit()],
+		server: {
+			allowedHosts: [
+				env.PUBLIC_SITE_DOMAIN
+			]
+		}
+	};
 });
