c4ef0a88aa08854c0513aef20073b9401b83b137
nikosmpi <nikosmpi@gmail.com>
2026-05-08 13:06:25 +0000

    intro page admin pt2

---
+++ src/routes/admin/intro-page/+page.server.js
@@ -0,0 +1,16 @@
+import { auth } from '$lib/server/auth';
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ request }) => {
+	const session = await auth.api.getSession({
+		headers: request.headers
+	});
+
+	if (!session || session.user.role !== 'admin') {
+		throw redirect(302, '/');
+	}
+
+	return {
+		user: session.user
+	};
+};
