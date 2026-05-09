004b9b3ae1f092f507aa814ae6e2dc048959286d
nikosmpi <nikosmpi@gmail.com>
2026-05-09 17:50:02 +0000

    login redirect

---
+++ src/routes/login/+page.server.js
@@ -0,0 +1,14 @@
+import { auth } from '$lib/server/auth';
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ request }) => {
+	const session = await auth.api.getSession({
+		headers: request.headers
+	});
+
+	if (session) {
+		throw redirect(302, '/');
+	}
+
+	return {};
+};
